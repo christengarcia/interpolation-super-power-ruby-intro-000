732ab6279f832909a45ac9b2c24291065ef470c6
Christen Garcia <christen.garcia@gmail.com>
2017-10-19 11:18:09 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,4 @@
-def display_rainbow(colors)
+def display_rainbow(colours)
   puts "R: #{colours[0]}, O: #{colours[1]}, Y: #{colours[2]}, G: #{colours]}, B: #{colours]}, I: #{colours[5]}, V: #{colours[6]}"
 end
+
